5dd5e482c7ab2f9ba88191d4d17728c6eb5ccfd3
Saptarshi <sapbhat2@in.ibm.com>
2018-09-28 11:15:02 +0200

    Removed DII information

--- AlienBotMailSender.js
+++ AlienBotMailSender.js
@@ -9,14 +9,14 @@ function sendMailFromAlienBot(conversation){
         auth: {
             //Add your email username and password here
             user: 'pmiwatsondemo',
-            pass: 'IBMwatson@5'
+            pass: '######' // Enter your email password here...
         }
     });
 
     // setup email data with unicode symbols
     let mailOptions = {
         from: '"Watson IBM" <pmiwatsondemo@gmail.com>', // sender address
-        to: 'aganguly@in.ibm.com', // list of receivers
+        to: '########', // list of receiver email
         subject: 'Hello - This ia an automated mail from starterbot ✔', // Subject line
         text: conversation, // plain text body
         //html: `<b>${conversation}</b>` // html body
